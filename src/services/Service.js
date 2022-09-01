701f74dfc558457ff77167ca8a1cab4d91b7b744
M. Seyfullah Bilgin <seyfullahbilgin06@gmail.com>
2022-09-01 18:01:52 +0300

    feat: add feature to monitor active page in url (#7)

--- src/services/Service.js
+++ src/services/Service.js
@@ -3,6 +3,9 @@ const API_BASE_URL = "https://rickandmortyapi.com/api/location"
 class Service {
 
   getAllLocations(activePage) {
+    const nextURL = `/?page=${activePage}`;
+    window.history.pushState({}, "", nextURL);
+
     return fetch(`${API_BASE_URL}/?page=${activePage}`, {
       method: "GET"
     })
