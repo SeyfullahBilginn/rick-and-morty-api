d2b39f6e762b9df1f415f7bb474e0278fe832a89
SeyfullahBilginn <seyfullahbilgin06@gmail.com>
2022-09-02 14:24:46 +0300

    feat: create grid and resident card for residents page

---
+++ src/Pages/ResidentCard.js
@@ -0,0 +1,76 @@
+import { shape, string } from "prop-types";
+import React from 'react'
+import { statusColors } from '../constants';
+import "./ResidentCard.css";
+
+export default function ResidentCard({ resident }) {
+  const {name, species, type, gender, origin, status} = resident;
+
+  return (
+    <div id="resident">
+      <div id="container">
+        <div id="title">
+          Name:&nbsp;
+        </div>
+        {name}
+      </div>
+      <div id="container">
+        <div id="title">
+          Species:&nbsp;
+        </div>
+        {species}
+      </div>
+      <div id="container">
+        <div id="title">
+          Type:&nbsp;
+        </div>
+        {type}
+      </div>
+      <div id="container">
+        <div id="title">
+          Gender:&nbsp;
+        </div>
+        {gender}
+      </div>
+      <div id="container">
+        <div id="title">
+          Origin:&nbsp;
+        </div>
+        {origin.name}
+      </div>
+      <div id="container">
+        <div id="title">
+          Status:&nbsp;
+        </div>
+        <div style={{ color: statusColors[status.toLowerCase()] }}>
+          {status}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+ResidentCard.propTypes = {
+  resident: shape({
+    name: string,
+    type: string,
+    species: string,
+    gender: string,
+    origin: shape({
+      name: string,
+      url: string
+    }),
+    status: string
+  })
+}
+
+ResidentCard.defaultProps = {
+  resident: {
+    name: "",
+    type: "",
+    species: "",
+    gender: "",
+    origin: { name: "", url: "" },
+    status: ""
+  }
+}
