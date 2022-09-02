aa544bece6d179ce72bd52572e55b8b28cce5880
M. Seyfullah Bilgin <seyfullahbilgin06@gmail.com>
2022-09-02 14:27:18 +0300

    feat: create grid and resident card for residents page (#13)

--- src/Pages/Residents.js
+++ src/Pages/Residents.js
@@ -1,40 +1,18 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import Service from "../services/Service";
-// import Service from "../services/Service";
 import "./Residents.css";
+import ResidentCard from "./ResidentCard";
 
 export default function Residents() {
   const location = useLocation();
   const [residents, setResidents] = useState([]);
-  const [counter, setCounter] = useState(0);
-  // function fetchResidents() {
-  // for (var i = 0; i < 100; i++) {
-  //   console.log("--");
-  //   setCounter(counter + 1);
-  // }
-  // console.log("fetchResidents");
-  // await location.state.residents.forEach(resident => {
-  //   Service.getResident(resident)
-  //     .then(res => res.json()
-  //       .then(result => {
-  //         console.log(result);
-  //         setResidents([...residents, result])
-  //       })
-  //       .catch(err => console.log(err))
-  //     )
-  //     .catch(err => console.log(err));
-  // })
-  // }
 
   async function get(url) {
-    console.log("---");
     Service.getResident(url)
       .then(res => res.json()
         .then(result => {
-          console.log(result);
           setResidents(prevState => [...prevState, result])
         })
         .catch(err => console.log(err))
@@ -45,7 +23,6 @@ export default function Residents() {
   async function requestAll() {
     return await Promise.all(location.state.residents.map(async url => {
       try {
-        console.log("+++");
         return await get(url);
       } catch (err) {
         console.log(err)
@@ -53,31 +30,17 @@ export default function Residents() {
     }));
   }
 
-
   useEffect(() => {
-    console.log("USE EFFECT");
-    console.log(location.state.id);
-    console.log(location.state.residents);
     // fetchResidents();
     requestAll();
   }, [])
 
 
   return (
-    <div className="parent">
-      <div>{location.state.id}</div>
-      <div>{location.state.residents.length}</div>
-      {counter}
-      <div>{residents.length}</div>
+    <div id="grid">
       {
-        residents[0] &&
-        <div>{residents[0].id}</div>
+        residents && residents.map(resident => <ResidentCard key={resident.id} resident={resident} />)
       }
-      {residents && residents.map((resident, index) => {
-        return (
-          <div key={index}>{resident.id}</div>
-        )
-      })}
     </div>
   );
 }
